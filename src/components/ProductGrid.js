b746026252fa028a58cd03ea77c96a94be5c1857
sedrak <sedrak005@gmail.com>
2021-04-22 18:12:52 +0300

    Product page and navbar changes

--- src/components/ProductGrid.js
+++ src/components/ProductGrid.js
@@ -1,18 +1,18 @@
 import React from "react";
-import Shop from "../assets/illu/Cart.svg";
+import ProductButton from "./ProductButton";
 
 const ProductsGrid = ({ items }) => {
   const renderItems = items.map((item) => {
     return (
       <div key={item.name} className="Item">
         <div className="image_wrapper">
-          <img className="product_image" src={item.image}></img>
-          <div className="text name">{item.name}</div>
+          <img className="product_image" src={item.image} />
+          <div className="text name toTop ">{item.name}</div>
+          <ProductButton href={item.path} />
         </div>
 
         <div className="item_buttons">
-          <div className="text price item_button">Buy {item.price}$</div>
-          <img className="add2cart item_button" src={Shop} />
+          <div className="text price item_button">${item.price}</div>
         </div>
       </div>
     );
